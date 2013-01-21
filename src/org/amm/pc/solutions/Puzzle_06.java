e8bcde6c5bf461626a66a53da5caf7fd17bac945
amm <andriy.amm@gmail.com>
2013-01-21 18:23:22 +0200

    working on puzzle_06

--- src/org/amm/pc/solutions/Puzzle_06.java
+++ src/org/amm/pc/solutions/Puzzle_06.java
@@ -1,15 +1,8 @@
 package org.amm.pc.solutions;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,53 +14,48 @@ public class Puzzle_06 {
 	public static final String PATTERN_PAIR = "(g(.+)a";
 	public static final String PATTERN_QUANTITY = "(I(\\d+)";
 	public static final String PATTERN_CHARACTER = "g(\\d)";
-	// public static final String URL_STRING =
-	// "http://www.pythonchallenge.com/pc/def/banner.p";
 	public static final String FILENAME = "peak.txt";
 
 	private class Pair {
-		char character;
-		int quantity;
+		private String character;
+		private int quantity;
 
-		public Pair(String symbol, int quantity2) {
-			// TODO Auto-generated constructor stub
-		}
-
-		public char getCharacter() {
-			return character;
-		}
-
-		public void setCharacter(char character) {
+		public Pair(String character, int quantity) {
 			this.character = character;
-		}
-
-		public int getQuantity() {
-			return quantity;
-		}
-
-		public void setQuantity(int quantity) {
 			this.quantity = quantity;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < quantity; i++) {
+				sb.append(character);
+			}
+
+			return sb.toString();
 		}
 	}
 
 	public void solution_0() {
 		try {
 			String sb = JCHelper.getFileContent(FILENAME).toString();
-
 			List<ArrayList<Pair>> banner = createBanner(sb);
 			printBanner(banner);
-			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
-	private void printBanner(List<ArrayList<Pair>> banner){
-		//TODO
+	private void printBanner(List<ArrayList<Pair>> banner) {
+		for (ArrayList<Pair> listPair : banner) {
+			for (Pair pair : listPair) {
+				System.out.print(pair.toString());
+			}
+			System.out.println();
+		}
 	}
-	
+
 	private List<ArrayList<Pair>> createBanner(String textBanner) {
 
 		List<ArrayList<Pair>> banner = new ArrayList<ArrayList<Pair>>();
